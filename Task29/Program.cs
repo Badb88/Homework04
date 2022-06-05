72ff12dc9a7a0c1a9c72be2c85693bf4c669ddde
OlegB <os.babkin@gmail.com>
2022-06-05 20:47:50 +0300

    Добавлено решение задачи на автоматическое заполнение, для ручного добавлены коментарии преобразования

---
+++ Task29/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    Console.WriteLine("Введите 8 элементов, после каждого нажмите Enter: ");
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = Convert.ToInt32(Console.ReadLine()); //new Random().Next(1,100);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    Console.Write("Массив из 8 элементов: [");
+    for (int pos = 0; pos < col.Length; pos++)
+    {
+        if (pos < col.Length - 1)
+        {
+            Console.Write($"{col[pos]}, ");
+        }
+        else
+        {
+            Console.Write(col[pos]);
+            break;
+        }
+    }
+    Console.Write("]");
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
